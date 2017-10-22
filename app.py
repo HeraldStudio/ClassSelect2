be1ad4caf04174bae3f626bad821300bbd6b6acb
vhyme <vhyme@live.cn>
2017-10-23 00:35:17 +0800

    初步完成

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ phone_re = re.compile(r'^1\d{10}$')
 
 
 class BaseHandler(RequestHandler):
-    executor = ThreadPoolExecutor(100)
+    executor = ThreadPoolExecutor(32)
 
     @property
     def db(self):
