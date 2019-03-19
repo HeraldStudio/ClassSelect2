2d225287f2f6c963ee91f0cbbf8d50e7ea0eb078
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-03-19 18:46:03 +0800

    修改名单

--- namelist.js
+++ namelist.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const xlsx = require('node-xlsx');
  
-const list = xlsx.parse("/Users/wolf_tungsten/Documents/金钥匙选课/2018/黑名单.xlsx");
+const list = xlsx.parse("/Users/wolf_tungsten/Documents/金钥匙选课/2019春/困难生申请信息.xls");
 users = {}
 list[0].data.forEach(k => {
-    users[k[2]] = true
+    users[k[0]] = {schoolnum:k[1], name:k[2]}
 });
-fs.writeFileSync('black.json',JSON.stringify(users))
+fs.writeFileSync('all.json',JSON.stringify(users))
