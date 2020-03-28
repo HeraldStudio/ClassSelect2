c71b67f6fb67911394f77fab4f2930377a32e636
ZZJ <zhaozjseu@163.com>
2020-03-28 23:28:52 +0800

    修改一下目录结构

--- namelist.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const xlsx = require('node-xlsx');
- 
-const list = xlsx.parse("/Users/wolf_tungsten/Documents/金钥匙选课/2019春/困难生申请信息.xls");
-users = {}
-list[0].data.forEach(k => {
-    users[k[0]] = {schoolnum:k[1], name:k[2]}
-});
-fs.writeFileSync('all.json',JSON.stringify(users))
